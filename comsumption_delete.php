b97c9ffbd008e6362d1088171a4431994b974567
Neelesh1209 <109412500+Neelesh1209@users.noreply.github.com>
2022-09-02 12:21:51 +0530

    Initial commit

---
+++ comsumption_delete.php
@@ -0,0 +1,20 @@
+<?php
+if ( isset($_GET["id"]) ) {
+    $id = $_GET["id"];
+
+    $servername = "epiz_32171591_v_guard";
+    $username = "epiz_32171591";
+    $password = "HdixsMJNWOwdZjQ";
+    $database = "v_guard";
+
+    // Create connection
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM diesel_comsumption WHERE id=$id";
+    $connection->query($sql);
+}
+
+header("location: comsumption.php");
+exit;
+
+?>
---
+++ logindb.php
@@ -0,0 +1,9 @@
+
+<?php
+	$servername = "epiz_32171591_v_guard";
+	$username = "epiz_32171591";
+	$password = "HdixsMJNWOwdZjQ";
+	$conn = mysqli_connect($servername, $username, $password);
+	$db = mysqli_select_db($conn,'v_guard');
+
+?>
